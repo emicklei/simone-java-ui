5a3ed5d11139e62cb4b9bb71c5cc9b042109509c
ernest <ernest@kramphub.com>
2023-01-08 15:24:00 +0100

    call eval and open window

--- src/main/java/com/ernestmicklei/EvalAction.java
+++ src/main/java/com/ernestmicklei/EvalAction.java
@@ -5,6 +5,10 @@ import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import javax.swing.*;
 import javax.swing.text.*;
 
@@ -18,5 +22,19 @@ public class EvalAction extends TextAction {
     }
     public void actionPerformed(ActionEvent e) {
         System.out.println(this._textArea.getSelectedText());
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest.BodyPublisher bp = HttpRequest.BodyPublishers.ofString(this._textArea.getSelectedText());
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:9119/v1?action=eval"))
+                .POST(bp)
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(this::handleEvalResponse)
+                .join();
+    }
+    private void handleEvalResponse(String json) {
+        new SimonUI().setVisible(true);
     }
 }
